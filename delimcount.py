17e58baab29a915043018ad371f88f6403676d5b
Stemur <Stemur@users.noreply.github.com>
2019-07-02 15:14:52 -0700

    CLI Arguments handled by argparse. Code tidy up

--- delimcount.py
+++ delimcount.py
@@ -1,69 +1,60 @@
 #!/usr/bin/python
 
-import sys, getopt
+import sys
 import locale
+import argparse
 
 locale.setlocale(locale.LC_NUMERIC, 'en_US')
 
-# Global Variables
-filesize = 0
-countonly = False
 
-def processfile(inputfile):
-	global countonly
-	global filesize
-	lineno = 1
-	try:
-		with open(inputfile, 'r') as f:
-			delimcount = 0
-			read_data = f.readline()
-			while read_data:
-				delimcount = read_data.count(',')
-				filesize += len(read_data)
-				outputdata(read_data, delimcount, lineno)
-				lineno += 1
-				read_data = f.readline()
-	except IOError as e:
-		print 'Error {0} : {1}'.format(e.errno, e.strerror)
-		sys.exit(2)
-	f.close()
+def processfile(inputfile, countonly):
+    filesize = 0
+    lineno = 1
+    try:
+        with open(inputfile, 'r') as f:
+            read_data = f.readline()
+            print('File Name : %s' % inputfile)
+            while read_data:
+                delimcount = read_data.count(',')
+                filesize += len(read_data)
+                outputdata(read_data, delimcount, lineno, countonly)
+                lineno += 1
+                read_data = f.readline()
+    except IOError as e:
+        print('Error {0} : {1}'.format(e.errno, e.strerror))
+        sys.exit(2)
+    f.close()
+    print('File size (bytes) : ' + locale.format_string('%i', filesize, grouping=True))
 
-def outputdata(linedata, delcount, lineno):
-	global countonly
-	print 'Line : {:<5}'.format(lineno), # % (lineno),
-	if countonly == False:
-		print ' {:<5}'.format(delcount),
-		print linedata.rstrip()
-	else:
-		print ' Delimiter Count : %i ' % (delcount)
 
-def main(argv):
-	global countonly
-	inputfile = ''
-	usgstr = 'Correct useage is: delimcount.py [-c] -i <inputfile>'
-	try:
-		opts, args = getopt.getopt(argv,"hci:",['ifile='])
-	except getopt.GetoptError:
-		print usgstr
-		sys.exit(2)
-	if len(opts) == 0:
-		print "No options supplied. %s" % usgstr
-		sys.exit()
-	for opt, arg in opts:
-		if opt == '-i':
-			inputfile = arg
-		elif opt == '-h':
-			print "delimcount.py [-c] -i <inputfile>"
-			sys.exit()
-		elif opt == '-c':
-			print 'Displaying delimiter counts for each line only.'
-			countonly = True
-	if len(inputfile) == 0:
-		print "No input file name supplied. %s" % usgstr
-		sys.exit()
-	print 'File Name : %s' % inputfile
-	processfile(inputfile)
-	print 'File size (bytes) : ' + locale.format('%i', filesize, grouping = True)
+def outputdata(linedata, delcount, lineno, countonly):
+    print('Line : {:<5}'.format(lineno), end='')
+    if not countonly:
+        print(' {:<5}'.format(delcount), end='')
+        print(linedata.rstrip())
+    else:
+        print(' Delimiter Count : %i ' % delcount)
+
+
+def main():
+    # Argument Parser
+    arg_parser = argparse.ArgumentParser(prog='delimcount',
+                                         usage='%(prog)s [options] -i filename',
+                                         description='count the number of delimiters on each line in a file')
+    arg_parser.add_argument('-c', '--count', action='store_true',
+                            dest='countonly',
+                            help='display delimiter counts on each line',
+                            required=False)
+    arg_parser.add_argument('-i', '--input', action='store',
+                            metavar='inputfile',
+                            dest='inputfile',
+                            type=str,
+                            help='the name of the file to be inspected',
+                            required=True)
+
+    args = arg_parser.parse_args()
+    processfile(args.inputfile, args.countonly)
+
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+    main()
